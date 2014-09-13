7fc8a82f28e86cf054bb789719840cc985082db1
Alex <alex@alexseifert.com>
2014-09-13 17:05:55 +0200

    Initial commit

--- README.md
+++ README.md
@@ -2,3 +2,96 @@ Basics.js
 =========
 
 A script with a few basic JavaScript functions
+
+Requirements
+------------
+
+- jQuery
+
+Browser Detection
+-----------------
+
+There is one method to call for getting the user's browser:
+
+- `bsGetBrowser()` : detects the type of browser and returns the following possible values: 
+..* `IE Mobile`
+..* `IE`
+..* `Firefox Mobile`
+..* `Firefox`
+..* `Android`
+..* `Chrome Mobile`
+..* `Chrome`
+..* `Opera Mini`
+..* `Opera Mobile`
+..* `Opera`
+..* `Safari Mobile`
+..* `Safari`
+..* `Blackberry`
+... If none of these browsers are detected, the user agent string is returned for parsing.
+
+Image Lazy Loading
+------------------
+
+There are three methods to call for lazy loading images:
+
+- `bsLazyLoadImages(fadeIn)` : fadeIn is true/false; this lazy loads all images on the page with the required parts in the image tag (see below)
+- `bsLazyLoadImagesWithOffset(fadeIn, offset)` : fadeIn is true/false; offset is the number of pixels outside of the viewport the image should load
+- `bsLazyLoadImage(img, fadeIn)` : load a specific image (must be passed as a jQuery object, i.e. `$('#myImage')`); fadeIn is true/false
+
+You should call these methods on window load, on window scroll, or whenever you want the images to load:
+
+```javascript
+	$(document).ready(function() {	 				
+		bsLazyLoadImages(true);
+	});
+```
+
+```javascript
+	$(window).scroll(function() {	 				
+		bsLazyLoadImagesWithOffset(false, 300);
+	});
+```
+
+```javascript
+	$('#myButton').click(function() {	 				
+		bsLazyLoadImages(false);
+	});
+```
+
+There are two parts that the image tag requires in order for the script to lazily load the images:
+
+- `class="bsLazyLoad"`
+- `data-src="path/to/image/to/be/loaded.jpg"`
+
+An example image tag:
+
+```html
+	<img src="blank.gif" data-src="image-to-load.jpg" class="bsLazyLoad">
+```
+
+
+Animated Scrolling
+------------------
+
+There are three methods for animated scrolling:
+
+- `bsScrollTo(px)` : scrolls vertically to a certain pixel on the page
+- `bsScrollToId(id)` : scrolls to an element with a certain id
+- `bsScrollToTop()` : scrolls to the top of the page
+
+
+Check if an element is in the viewport or is about to come into the viewport
+----------------------------------------------------------------------------
+
+There are two methods for checking if an element is already in the viewport or is about to come into the viewport:
+
+- `bsIsScrolledIntoView(elem)` : always checks whether the passed element is fully in the viewport with no offset
+- `bsIsScrolledIntoViewWithOffset(elem, offset)` : checks whether the passed element is in the viewport or is about to enter the viewport based on the passed offset in pixels
+
+
+Check if device is a touch screen
+----------------------------------
+
+There is one method which checks whether the user's device is a touch device or not:
+
+- `bsIsTouch()` : returns true or false
---
+++ basics.js
@@ -0,0 +1,165 @@
+/*
+*	Basics.js
+*	Version 0.1
+*	Copyright (c) Alex Seifert 2014
+*	http://www.alexseifert.com
+*	https://github.com/eiskalteschatten/Basics.js
+*/
+
+window.onload = function() {
+
+	// Check for jQuery & load from Google CDN if not available
+
+	if (typeof jQuery == 'undefined') {
+		var jq = document.createElement('script'); jq.type = 'text/javascript';
+		jq.src = '//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js';
+		document.getElementsByTagName('head')[0].appendChild(jq);
+	} 
+}
+
+
+// Browser detection
+//		- Following browsers will be detected: Internet Explorer (desktop & mobile), Firefox, Chrome, Chrome mobile, Opera (desktop, mini, mobile), Safari, Safari mobile, Android browser, Blackberry
+
+function bsGetBrowser() {
+	var objAgent = navigator.userAgent;
+	
+	if (objAgent.indexOf("IEMobile") != -1) {
+		return "IE Mobile";
+	}
+	else if (objAgent.indexOf("MSIE") != -1 || objAgent.indexOf("Trident") != -1) {
+		return "IE";
+	}
+	else if (objAgent.indexOf("Firefox") != -1 && bsIsTouch()) {
+		return "Firefox Mobile";
+	}
+	else if (objAgent.indexOf("Firefox") != -1) {
+		return "Firefox";
+	}
+	else if (objAgent.indexOf("Android") != -1) {
+		return "Android";
+	}
+	else if (objAgent.indexOf("Chrome") != -1 && bsIsTouch()) {
+		return "Chrome Mobile";
+	}
+	else if (objAgent.indexOf("Chrome") != -1) {
+		return "Chrome";
+	}
+	else if (objAgent.indexOf("Opera Mini") != -1) {
+		return "Opera Mini";
+	}
+	else if (objAgent.indexOf("Opera Mobi") != -1) {
+		return "Opera Mobile";
+	}
+	else if (objAgent.indexOf("Opera") != -1) {
+		return "Opera";
+	}
+	else if (objAgent.indexOf("Safari") != -1 && bsIsTouch()) {
+		return "Safari Mobile";
+	}
+	else if (objAgent.indexOf("Safari") != -1) {
+		return "Safari";
+	}
+	else if (objAgent.indexOf("Blackberry") != -1) {
+		return "Blackberry";
+	}
+	else {
+		return objAgent;
+	}
+}
+
+
+// Animated Scrolling
+
+function bsScrollTo(px) {
+	$('html, body').animate({scrollTop: px+'px'}, 2000, 'easeInOutCubic');
+}
+
+function bsScrollToId(id) {
+	if (id.indexOf('#') == -1) {
+		id = "#" + id;
+	}
+	
+	$('html, body').animate({scrollTop: $(id).offset().top}, 2000, 'easeInOutCubic');
+}
+
+function bsScrollToTop() {
+	bsScrollTo('0');
+}
+
+
+// Check if an element is in the viewport
+
+function bsIsScrolledIntoView(elem) {
+	return bsIsScrolledIntoViewWithOffset(elem, 0);
+}
+
+
+// Check if an element is in the viewport or is about to come into the viewport with an offset
+
+function bsIsScrolledIntoViewWithOffset(elem, offset) {
+    var docViewTop = $(window).scrollTop();
+    var docViewBottom = $(window).scrollTop() + $(window).height();
+
+	if (!isNaN(offset)) {
+		docViewTop -= parseInt(offset);
+		docViewBottom += parseInt(offset);
+	}
+
+    var elemTop = $(elem).offset().top;
+    var elemBottom = elemTop + $(elem).height();
+
+    return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
+}
+
+
+// Image lazy loading
+// 		- All images that should be lazily loaded require the class "bsLazyLoad" and 
+//		  the attribute "data-src" with the path to the image
+//		- fadeIn: true/false - Images will fade in after loading
+
+function bsLazyLoadImages(fadeIn) {
+	$('img.bsLazyLoad').each(function() {
+		bsLazyLoadImage($(this), fadeIn);
+	});
+}
+
+// Lazy load images with an offset
+//		- offset: Pixel offset for when the images should be loaded
+
+function bsLazyLoadImagesWithOffset(fadeIn, offset) {
+	$('img.bsLazyLoad').each(function() {
+		if (bsIsScrolledIntoViewWithOffset($(this), offset)) {
+			bsLazyLoadImage($(this), fadeIn);
+		}
+	});
+}
+
+function bsLazyLoadImage(img, fadeIn) {
+	var lazySrc = img.attr('data-src');
+	var newImg = $("<img />").attr('src', lazySrc).load(function() {
+        if (!this.complete || typeof this.naturalWidth == "undefined" || this.naturalWidth == 0) {
+            console.log("Image not found: "+lazySrc);
+        }
+        else {
+	    	img.css('opacity', 0);
+            img.attr('src', lazySrc);
+            img.removeClass('bsLazyLoad');
+            img.removeAttr('data-src');
+	        
+	        if (fadeIn) {
+		    	img.animate({opacity: 1});
+	        }
+	        else {
+    	    	img.css('opacity', 1);
+			}
+        }
+    });
+}
+
+
+// Check if device is a touch screen
+
+function bsIsTouch() {
+	return (('ontouchstart' in window) || (navigator.MaxTouchPoints > 0) || (navigator.msMaxTouchPoints > 0));
+}
